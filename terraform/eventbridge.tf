3439f0419d4dd0cfaf7317be743369467a611a16
vigalcon <vgalcon92+github@gmail.com>
2026-06-23 19:21:09 +0100

    feat: delete event rule trigger

--- terraform/eventbridge.tf
+++ terraform/eventbridge.tf
@@ -7,55 +7,57 @@
 # -----------------------------------------------------------------------------
 # EventBridge Rule - S3 Object Created
 # -----------------------------------------------------------------------------
-resource "aws_cloudwatch_event_rule" "s3_trigger" {
-  count       = local.s3_trigger.enabled ? 1 : 0
-  name        = "${local.full_name}-s3-trigger"
-  description = "Trigger Step Function when PDF is uploaded to S3"
-
-  event_pattern = jsonencode({
-    source      = ["aws.s3"]
-    detail-type = ["Object Created"]
-    detail = {
-      bucket = {
-        name = [local.datalake.raw.bucket_name]
-      }
-      object = {
-        key = concat(
-          local.s3_trigger.prefix != "" ? [{ prefix = local.s3_trigger.prefix }] : [],
-          local.s3_trigger.suffix != "" ? [{ suffix = local.s3_trigger.suffix }] : []
-        )
-      }
-    }
-  })
-
-  tags = local.common_tags
-}
+# COMMENTED OUT FOR DELETION AND RECREATION
+# resource "aws_cloudwatch_event_rule" "s3_trigger" {
+#   count       = local.s3_trigger.enabled ? 1 : 0
+#   name        = "${local.full_name}-s3-trigger"
+#   description = "Trigger Step Function when the events are created in S3"
+#
+#   event_pattern = jsonencode({
+#     source      = ["aws.s3"]
+#     detail-type = ["Object Created"]
+#     detail = {
+#       bucket = {
+#         name = [local.datalake.raw.bucket_name]
+#       }
+#       object = {
+#         key = concat(
+#           local.s3_trigger.prefix != "" ? [{ prefix = local.s3_trigger.prefix }] : [],
+#           local.s3_trigger.suffix != "" ? [{ suffix = local.s3_trigger.suffix }] : []
+#         )
+#       }
+#     }
+#   })
+#
+#   tags = local.common_tags
+# }
 
 # -----------------------------------------------------------------------------
 # EventBridge Target - Step Function
 # -----------------------------------------------------------------------------
-resource "aws_cloudwatch_event_target" "step_function" {
-  count     = local.s3_trigger.enabled ? 1 : 0
-  rule      = aws_cloudwatch_event_rule.s3_trigger[0].name
-  target_id = "TriggerStepFunction"
-  arn       = aws_sfn_state_machine.main.arn
-  role_arn  = aws_iam_role.eventbridge[0].arn
-
-  input_transformer {
-    input_paths = {
-      bucket    = "$.detail.bucket.name"
-      key       = "$.detail.object.key"
-      size      = "$.detail.object.size"
-      eventTime = "$.time"
-    }
-    input_template = <<EOF
-{
-  "bucket": <bucket>,
-  "key": <key>,
-  "size": <size>,
-  "eventTime": <eventTime>,
-  "environment": "${var.environment}"
-}
-EOF
-  }
-}
+# COMMENTED OUT FOR DELETION AND RECREATION
+# resource "aws_cloudwatch_event_target" "step_function" {
+#   count     = local.s3_trigger.enabled ? 1 : 0
+#   rule      = aws_cloudwatch_event_rule.s3_trigger[0].name
+#   target_id = "TriggerStepFunction"
+#   arn       = aws_sfn_state_machine.main.arn
+#   role_arn  = aws_iam_role.eventbridge[0].arn
+#
+#   input_transformer {
+#     input_paths = {
+#       bucket    = "$.detail.bucket.name"
+#       key       = "$.detail.object.key"
+#       size      = "$.detail.object.size"
+#       eventTime = "$.time"
+#     }
+#     input_template = <<EOF
+# {
+#   "bucket": <bucket>,
+#   "key": <key>,
+#   "size": <size>,
+#   "eventTime": <eventTime>,
+#   "environment": "${var.environment}"
+# }
+# EOF
+#   }
+# }
